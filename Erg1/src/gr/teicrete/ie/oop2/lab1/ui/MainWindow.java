9acdbf38e890e9d7df55b75283308748f2dcecb3
manosgem <manosgem@debian.dyn6.edudsl.gr>
2017-10-13 00:35:14 +0300

    add/rem buttons, colour change, last button label. buttonSelected label is trigger.

--- Erg1/src/gr/teicrete/ie/oop2/lab1/ui/MainWindow.java
+++ Erg1/src/gr/teicrete/ie/oop2/lab1/ui/MainWindow.java
@@ -2,13 +2,16 @@ package gr.teicrete.ie.oop2.lab1.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Hashtable;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -19,151 +22,200 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.font.TextAttribute;
+import java.util.Map;
 
 /**
  *
  * @author manosgem
  */
 public class MainWindow extends JFrame {
-
+    
     private JPanel mainPanel;
     private JPanel sliderPanel;
     private JPanel colourPanel;
     private JPanel colourPanell;
     private JPanel colourPanelr;
     private JPanel infoPanel;
-
+    
     private JLabel bgColourLabel;
     private JLabel bgColourPreview;
     private JLabel fgColourLabel;
     private JLabel fgColourPreview;
     private JLabel buttonSelectedTextLabel;
     private JLabel buttonSelectedLabel;
-
+    
     private JColorChooser bgColourChooser;
     private JColorChooser fgColourChooser;
-
+    
     private JSlider slider;
-
+    
     private ActionListener buttonSelectedAL;
+    private MouseListener buttonShadowML;
     private int pperc = 0;
-
+    
     public MainWindow() {
-
+        
         mainPanel = new JPanel();
         sliderPanel = new JPanel();
         colourPanel = new JPanel();
         colourPanell = new JPanel();
         colourPanelr = new JPanel();
         infoPanel = new JPanel();
-
+        
         bgColourLabel = new JLabel("Background Colour:");
         bgColourPreview = new JLabel("     ");
         fgColourLabel = new JLabel("Foreground Colour:");
         fgColourPreview = new JLabel("     ");
         buttonSelectedTextLabel = new JLabel("ButtonSelected:");
         buttonSelectedLabel = new JLabel();
-
+        
+        Font font = new Font("Sheriff", Font.ITALIC, 12);
+        
         slider = new JSlider(JSlider.VERTICAL, 0, 100, 0);
-
+        
+        buttonSelectedAL = new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e) {
+                
+                Object source = e.getSource();
+                
+                if (source instanceof JButton) {
+                    buttonSelectedLabel.setText(((JButton) source).getText());
+                }
+            }
+        };
+        
+        buttonShadowML = new MouseListener() {
+            public void mouseEntered(MouseEvent e) {
+                for (int i = 0; i < mainPanel.getComponentCount(); i++) {
+                    
+                    if (mainPanel.getComponent(i) instanceof JButton) {
+                        if (((JButton) mainPanel.getComponent(i)).getText() == buttonSelectedLabel.getText()) {
+                            ((JButton) mainPanel.getComponent(i)).setBorderPainted(false);
+                        }                        
+                    }
+                }
+            }
+            
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+            
+            @Override
+            public void mousePressed(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent me) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+            
+            public void mouseExited(MouseEvent me) {
+                for (int i = 0; i < mainPanel.getComponentCount(); i++) {
+                    
+                    if (mainPanel.getComponent(i) instanceof JButton) {
+                        if (((JButton) mainPanel.getComponent(i)).getText() == buttonSelectedLabel.getText()) {
+                           ((JButton) mainPanel.getComponent(i)).setBorderPainted(true);
+                        }                        
+                    }
+                }
+            }
+        };
+        
         colourPanel.setLayout(new GridLayout(1, 2));
-
+        
         ((FlowLayout) colourPanell.getLayout()).setAlignment(FlowLayout.LEFT);
         ((FlowLayout) colourPanelr.getLayout()).setAlignment(FlowLayout.LEFT);
-
+        
         bgColourPreview.setBackground(new JButton().getBackground());
         bgColourPreview.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         bgColourPreview.setOpaque(true);
-
+        
         bgColourPreview.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 Color bgColour = JColorChooser.showDialog(bgColourPreview, "BackGround Colour:", Color.white);
-
+                
                 bgColourPreview.setBackground(bgColour);
-                for(int i=0;i<mainPanel.getComponentCount();i++){
+                for (int i = 0; i < mainPanel.getComponentCount(); i++) {
                     mainPanel.getComponent(i).setBackground(bgColour);
                 }
                 mainPanel.revalidate();
                 mainPanel.repaint();
             }
         });
-
+        
         colourPanelr.add(bgColourLabel);
         colourPanelr.add(bgColourPreview);
-
+        
         fgColourPreview.setBackground(new JButton().getForeground());
         fgColourPreview.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         fgColourPreview.setOpaque(true);
-
+        
         fgColourPreview.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 Color fgColour = JColorChooser.showDialog(fgColourPreview, "Foreground Colour:", Color.white);
-
+                
                 fgColourPreview.setBackground(fgColour);
-                for(int i=0;i<mainPanel.getComponentCount();i++){
+                for (int i = 0; i < mainPanel.getComponentCount(); i++) {
                     mainPanel.getComponent(i).setForeground(fgColour);
                 }
                 mainPanel.revalidate();
                 mainPanel.repaint();
             }
         });
-
+        
         colourPanell.add(fgColourLabel);
         colourPanell.add(fgColourPreview);
-
+        
         colourPanel.add(colourPanell);
         colourPanel.add(colourPanelr);
-
+        
         add(colourPanel, BorderLayout.NORTH);
-
+        
         ((FlowLayout) infoPanel.getLayout()).setAlignment(FlowLayout.LEFT);
-
+        
+        Map attributes = font.getAttributes();
+        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+        buttonSelectedLabel.setFont(font.deriveFont(attributes));
+        buttonSelectedLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        buttonSelectedLabel.addMouseListener(buttonShadowML);
         infoPanel.add(buttonSelectedTextLabel);
         infoPanel.add(buttonSelectedLabel);
-
+        
         add(infoPanel, BorderLayout.SOUTH);
-
+        
         ((FlowLayout) mainPanel.getLayout()).setAlignment(FlowLayout.LEFT);
-
-        buttonSelectedAL = new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-
-                Object source = e.getSource();
-
-                if (source instanceof JButton) {
-                    buttonSelectedLabel.setText(((JButton) source).getText());
-                }
-            }
-        };
-
+        
         add(mainPanel, BorderLayout.CENTER);
-
+        
         sliderPanel.setLayout(new GridLayout(0, 1));
-
+        
         slider.setMajorTickSpacing(10);
         slider.setMinorTickSpacing(5);
         slider.setPaintTicks(true);
-
+        
         Hashtable labelTable = new Hashtable();
         for (int i = 0; i < 101; i += 5) {
             labelTable.put(new Integer(i), new JLabel(Integer.toString(i)));
         }
-
+        
         slider.setLabelTable(labelTable);
         slider.setPaintLabels(true);
         slider.setSnapToTicks(true);
-
+        
         slider.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 JSlider source = (JSlider) e.getSource();
                 if (!source.getValueIsAdjusting()) {
                     int perc = source.getValue();
-
+                    
                     if (perc > pperc) {
                         
-                        for(int i =1; i<=perc-pperc; i++ ){
-                            JButton button = new JButton("Button "+(pperc+i));
+                        for (int i = 1; i <= perc - pperc; i++) {
+                            JButton button = new JButton("Button " + (pperc + i));
                             
                             button.addActionListener(buttonSelectedAL);
                             button.setBackground(bgColourPreview.getBackground());
@@ -174,8 +226,8 @@ public class MainWindow extends JFrame {
                         pperc = perc;
                     } else if (perc < pperc) {
                         
-                        for(int j= pperc; j>perc;j--){
-                            mainPanel.remove(mainPanel.getComponent(mainPanel.getComponentCount()-1));
+                        for (int j = pperc; j > perc; j--) {
+                            mainPanel.remove(mainPanel.getComponent(mainPanel.getComponentCount() - 1));
                         }
                         pperc = perc;
                     }
@@ -185,11 +237,11 @@ public class MainWindow extends JFrame {
                 }
             }
         });
-
+        
         sliderPanel.add(slider);
-
+        
         add(sliderPanel, BorderLayout.EAST);
-
+        
         pack();
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
