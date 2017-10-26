b419d7a5b63d3102ca9a6110635c45a6c1279769
labuser <labuser@Lab4-PC-PC>
2017-10-26 17:10:20 +0300

    fix imports

--- Erg1/src/gr/teicrete/ie/oop2/lab1/ui/MainWindow.java
+++ Erg1/src/gr/teicrete/ie/oop2/lab1/ui/MainWindow.java
@@ -3,7 +3,6 @@ package gr.teicrete.ie.oop2.lab1.ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -11,7 +10,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.Hashtable;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -24,7 +22,6 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.font.TextAttribute;
 import java.util.Map;
-import javax.swing.UIManager;
 
 /**
  *
